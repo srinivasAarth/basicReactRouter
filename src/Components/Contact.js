74c6e67596e897bd5740bb63dae8452bcaf73941
Srinivas Chary <srinivasa@aarth.io>
2022-10-02 13:21:33 +0530

    fix cookie session

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -3,23 +3,27 @@ import { Route, Routes, useNavigate } from "react-router";
 import ContactsOne from "./Contacts/ContactsOne";
 import ContactsThree from "./Contacts/ContactsThree";
 import ContactsTwo from "./Contacts/ContactsTwo";
+import Cookies from "universal-cookie";
 
 const Contact = () => {
   const navigate = useNavigate();
+
+  const cookies = new Cookies();
+  cookies.set("myCat", { Name: "don" });
+  console.log(cookies.get("myCat"));
+
   return (
     <>
       <div>this is Contacts page</div>
       <button onClick={() => navigate("ContactsOne")}>ContactsOne</button>
       <button onClick={() => navigate("ContactsTwo")}>ContactsTwo</button>
       <button onClick={() => navigate("ContactsThree")}>ContactsThree</button>
-      <button onClick={() => navigate("ContactsLast")}>ContactsThree</button>
 
       <Routes>
         <Route path="/" element={<ContactsOne />} />
         <Route path="/ContactsOne" element={<ContactsOne />} />
         <Route path="/ContactsTwo" element={<ContactsTwo />} />
         <Route path="/ContactsThree" element={<ContactsThree />} />
-        <Route path="/ContactsLast" element={<ContactsTwo />} />
       </Routes>
     </>
   );
